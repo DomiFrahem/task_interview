c3d3137f8c3a0a0ca38ef08b034e20aa0412cce9
Alex Sidorov <for_kbsu@alexsid.ru>
2022-10-03 15:43:54 +0300

    Добавлена проверка по маске, и изменен json на отдачу

--- core/add.php
+++ core/add.php
@@ -1,41 +1,52 @@
-<?php 
-    require_once "config.php";
-    require "./DB.php";
-     
-    function add_hardware($id_type, $serial_number){        
-        $db = new DB();
-        $connect = $db->connect();
-        
-        $query = "INSERT INTO hardware (id_type, serial_number) VALUES (:id_type, :serial_number)";
+<?php
+require_once __DIR__ . "/config.php";
+require __DIR__ . "/DB.php";
+require __DIR__ . "/Mask.php";
 
-        try {
-            $stmt = $connect->prepare($query);
-            $stmt->bindValue("id_type", $id_type, PDO::PARAM_INT);
-            $stmt->bindValue("serial_number", $serial_number, PDO::PARAM_STR);
-            $stmt->execute();
-            return true; 
-        } catch (PDOException $e){
-            // print($e);
-            return false;
-        }
+
+function add_hardware($id_type, $serial_number)
+{
+    $db = new DB();
+    $connect = $db->connect();
+
+    $query = "INSERT INTO hardware (id_type, serial_number) VALUES (:id_type, :serial_number)";
+
+    try {
+        $stmt = $connect->prepare($query);
+        $stmt->bindValue("id_type", $id_type, PDO::PARAM_INT);
+        $stmt->bindValue("serial_number", $serial_number, PDO::PARAM_STR);
+        $stmt->execute();
+        return true;
+    } catch (PDOException $e) {
+        return false;
     }
+}
 
-    if (isset($_POST['type'])) $id_type = $_POST['type'];
-    if (isset($_POST['serial_number'])) $serial_number = $_POST['serial_number'];
-    
-    $error_serial_number = array();
-    $good_serial_number = array();
+if (isset($_POST['type']))
+    $id_type = $_POST['type'];
+if (isset($_POST['serial_number']))
+    $serial_number = $_POST['serial_number'];
 
-    foreach($serial_number as $number){
-        if (add_hardware($id_type, $number)){
-            array_push($good_serial_number, $number);
-        } else {
-            array_push($error_serial_number, $number);
-        }
+$mask = new Mask($id_type["mask_ns"]);
+
+list(
+    $matched_serial_numbers,
+    $no_matched
+) = $mask->many_equals($serial_number);
+
+$dublicate_serial_number = array();
+$good_serial_number = array();
+
+foreach ($matched_serial_numbers as $number) {
+    if (add_hardware($id_type, $number)) {
+        array_push($good_serial_number, $number);
+    } else {
+        array_push($dublicate_serial_number, $number);
     }
+}
 
-    echo json_encode(array(
-        "good" => $good_serial_number,
-        "error" => $error_serial_number
-    ));
-        
+echo json_encode(array(
+    "good" => $good_serial_number,
+    "dublicate" => $dublicate_serial_number,
+    "no_match" => $no_matched
+));

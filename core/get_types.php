3418fde80da01893029d391d8c48c6e74f40af8e
Alex Sidorov <for_kbsu@alexsid.ru>
2022-10-03 15:36:03 +0300

    Добавлен __DIR__

--- core/get_types.php
+++ core/get_types.php
@@ -1,22 +1,24 @@
 <?php
-   
-    require_once "config.php";
-    require "./DB.php";
-    
-   
-    class TypesHardware {
-        public $id, $name_type, $mask_ns;
-    }
 
-    
-    function get_type_from_db(){        
-        $db = new DB();
-        $connect = $db->connect();
-        
-        $query = "SELECT * FROM types_hardware";
-        $stmt = $connect->prepare($query);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_CLASS, "TypesHardware");
-    }
+require_once __DIR__ . "/config.php";
+require __DIR__ . "/DB.php";
 
-    echo json_encode(get_type_from_db());
+
+class TypesHardware
+{
+    public $id, $name_type, $mask_ns;
+}
+
+
+function get_type_from_db()
+{
+    $db = new DB();
+    $connect = $db->connect();
+
+    $query = "SELECT * FROM types_hardware";
+    $stmt = $connect->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_CLASS, "TypesHardware");
+}
+
+echo json_encode(get_type_from_db());
